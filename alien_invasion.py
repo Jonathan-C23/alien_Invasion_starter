9392ee424a47f8262e4ea0bd216c59fce1f628ff
jonathan-carpenter-arisen <jonathan.carpenter@arisen.com>
2025-12-14 12:20:29 -0500

    Added ship class to game. The game draws the ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     def __init__(self):
@@ -18,6 +19,8 @@ class AlienInvasion:
         self.running = True
         self.clock = pygame.time.Clock()
 
+        self.ship = Ship(self)
+
     def run_game(self):
         # Game loop
         while self.running:
@@ -26,7 +29,10 @@ class AlienInvasion:
                     self.running = False
                     pygame.quit()
                     sys.exit()
+
             self.screen.blit(self.bg, (0,0))
+            
+            self.ship.draw()
             pygame.display.flip()
             self.clock.tick(60)
 
