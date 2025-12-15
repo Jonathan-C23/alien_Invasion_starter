85062a04bf5cba597775c40945086a05e2d101bd
jonathan-carpenter-arisen <jonathan.carpenter@arisen.com>
2025-12-14 23:16:29 -0500

    P3 - Added high score saving

--- game_status.py
+++ game_status.py
@@ -1,3 +1,4 @@
+import json
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -10,6 +11,28 @@ class GameStatus():
         self.max_score = 0
         self.score = 0
         self.level = 1
+        self.init_saved_scores()
+        self.reset_stats()
+
+    def init_saved_scores(self):
+        self.path = self.settings.scores_file
+        if self.path.exists() and self.path.stat.__sizeof__() > 80:
+            contents = self.path.read_text()
+            scores = json.loads(contents)
+            self.hi_score = scores.get('hi_score', 0)
+        else:
+            self.hi_score = 0
+            self.save_scores()
+    
+    def save_scores(self):
+        scores = {
+            'hi_score': self.hi_score
+        }
+        contents = json.dumps(scores, indent=4)
+        try:
+            self.path.write_text(contents)
+        except FileNotFoundError as e:
+            print(f'File Not Found: {e}')
 
     def reset_stats(self):
         self.ships_left = self.settings.starting_ship_count
@@ -19,10 +42,15 @@ class GameStatus():
     def update(self, collisions):
         self._update_score(collisions)
         self._update_max_score()
+        self._update_high_score()
 
     def _update_max_score(self):
         if self.score > self.max_score:
             self.max_score = self.score
+
+    def _update_high_score(self):
+        if self.score > self.hi_score:
+            self.hi_score = self.score
 
     def _update_score(self, collisions):
         for alien in collisions.values():
